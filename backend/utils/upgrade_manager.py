46cc7f1e40374f0ae69bac5b96d7636a903ba6e0
qq85423296 <85423296@qq.com>
2026-01-05 09:27:08 +0800

    更新版本号为2.0.2

--- backend/utils/upgrade_manager.py
+++ backend/utils/upgrade_manager.py
@@ -17,7 +17,7 @@ class UpgradeManager:
     """升级管理器"""
     
     # 当前版本号
-    CURRENT_VERSION = "2.0.1"
+    CURRENT_VERSION = "2.0.2"
     
     # 备份目录
     BACKUP_DIR = Path(__file__).parent.parent.parent / 'backups'
